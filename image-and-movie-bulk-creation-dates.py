5681871f8a810de03c7f1a342ae40f1758b68a3c
seljasen <hseljaasen@hotmail.com>
2020-10-11 00:39:59 +0200

    Cleanup finished. Included setctime functionality directly in the code.

--- image-and-movie-bulk-creation-dates.py
+++ image-and-movie-bulk-creation-dates.py
@@ -1,37 +1,26 @@
 import argparse
-import site
 import os
-import tempfile
-import shutil
-import zipfile
-import logging
-import datetime
-from win32_setctime import setctime
-
-
 import time
 import datetime
 import calendar
-from ctypes import windll, wintypes, byref
-import win32.lib.win32con as win32con
-import win32file 
-import pywintypes
 import piexif
+from ctypes import windll, wintypes, byref
 
 
 #Photo:
-# 1. Look for a date in the given folder name.
-    # 2. If date is given, look for subfolder with date in name. if found, run self on this folder first.
-    # 3. For all files in folder Set creation date, and probably modification date if desired 
-    #       like the date from the folder name.
-# 4. If no date is found in root folder name, run self on all subfolders, 
-#       and do nothing with the files in current folder
-
-
-
+# Runs over entire directory hunting for folders starting with YYYY-MM-DD format
+# For all image files inside such a folder, the creation time and image taken time is 
+# edited accordingly
+#Movies
+#Not yet implemented.
 
 def handleFilesInFolder(root_folder):
     filelist = []
+    print('\n\n\n---------------------------------- Image Creation Date Update -----------------------------')
+    print('\nRoot folder: {root_folder}\n\nHandling subfolders:'.format(root_folder=root_folder))
+    image_count = 0
+    skipped_count = 0
+    failing_folders = []
     for root, dirs, files in os.walk(root_folder):
         for file in files:
             #print all the file names
@@ -41,23 +30,49 @@ def handleFilesInFolder(root_folder):
                 filelist.append(full_path)
                 folder_date = getDateTimeFolderPath(root)
                 epoch = getEpocFromDateTime(folder_date)
-                setctime(full_path, epoch)
+                if epoch is None:
+                    if root not in root_folder:
+                        skipped_count = skipped_count + 1
+                        if root not in failing_folders:
+                            failing_folders.append(root)
+                    continue
+                setCreationTime(full_path, epoch)
                 setImageDateTakenAttribute(full_path, folder_date)
-                print('Files:')    
-#        for name in files:
-#            print(name)
-#        print('Root:')  
-#        print(root)
-#        print('Directories:')    
-#        for directory in dirs:
-#            print(directory)  
-#        print('Filelist')
-#        for file in filelist:
-#            print(file)  
+                setLastModifiedTime(full_path, epoch)
+                image_count = image_count + 1
+        for dir in dirs:
+            print(dir)  
+
+    print('\nDone!\n{count} images updated.\n{skipped_count} images skipped.\n'.
+        format(count=image_count, skipped_count=skipped_count))    
+    if len(failing_folders) > 0:
+        print('Skipped folders:')
+    for folder in failing_folders:
+        print(folder) 
+    print('')
+    os.system("pause")
+
+def setLastModifiedTime(filepath, epochtime):
+    now_epoch = datetime.datetime.now().timestamp()
+    os.utime(filepath, (now_epoch, epochtime))
+
+## From https://stackoverflow.com/a/56805533/3407324
+def setCreationTime(filepath, epochtime):
+    # Convert Unix timestamp to Windows FileTime using some magic numbers
+    # See documentation: https://support.microsoft.com/en-us/help/167296
+    timestamp = int((epochtime * 10000000) + 116444736000000000)
+    ctime = wintypes.FILETIME(timestamp & 0xFFFFFFFF, timestamp >> 32)
+    # Call Win32 API to modify the file creation date
+    handle = windll.kernel32.CreateFileW(filepath, 256, 0, None, 3, 128, None)
+    windll.kernel32.SetFileTime(handle, byref(ctime), None, None) #(handle, ctime, atime, mtime)
+    windll.kernel32.CloseHandle(handle)
+
 
 def setImageDateTakenAttribute(filename, date_time):
     exif_dict = piexif.load(filename)
-    exif_dict['Exif'] = { piexif.ExifIFD.DateTimeOriginal: datetime.datetime(*date_time[:6]).strftime("%Y:%m:%d %H:%M:%S") } #"%Y:%m:%d %H:%M:%S"
+    exif_dict['Exif'] = { 
+        piexif.ExifIFD.DateTimeOriginal: datetime.datetime(*date_time[:6]).strftime("%Y:%m:%d %H:%M:%S") 
+    } 
     exif_bytes = piexif.dump(exif_dict)
     piexif.insert(exif_bytes, filename)
 
@@ -66,47 +81,37 @@ def getDateTimeFolderPath(folder_path):
     folder_name = os.path.basename(folder_path)
     first_info = str.split(folder_name)[0]
     date_info = str.split(first_info,'-')
-    print(first_info)
     if len(date_info) > 2:
-        year = int(date_info[0])
-        month = int(date_info[1])
-        date = int(date_info[2])
+        year = date_info[0]
+        if 'x' in year.lower():
+            return None
+
+        month = date_info[1].lower().replace('xx','01').replace('x','1') #.replace('hh','09').replace('vv','03')
+        if not month.isnumeric():
+            return None
+
+        date = date_info[2].lower().replace('xx','01').replace('x','1')
+        if not date.isnumeric():
+            return None
+
         dateString = '{date}:{month}:{year}'.format(date=date, month=month, year=year)
-        #timestring = '{date}:{month}:{year} 00:00:00'.format(date=date, month=month, year=year)
         return time.strptime(dateString, "%d:%m:%Y");
     else:
-        return time.gmtime(0)
+        return None
 
-def getEpocFromFolderPath(folder_path):
-    timeshift = getUtcTimeDiff()
-    datetime = getDateTimeFolderPath(folder_path)
-    return calendar.timegm(datetime) + timeshift          
 
 def getEpocFromDateTime(datetime):
+    if datetime is None:
+        return None
     timeshift = getUtcTimeDiff()
-    return calendar.timegm(datetime) + timeshift        
+    return calendar.timegm(datetime) + timeshift 
+
 
 def getUtcTimeDiff():
     utcTime = datetime.datetime.utcnow().timestamp()
     localTime = datetime.datetime.now().timestamp()
     return utcTime - localTime   
 
-def main(
-    src_root_path,
-    dest_root_path,
-    has_aux_well,
-):
-    print('Source folder:')
-    print(src_root_path)
-    logger = logging.getLogger('creation_date_edit')
-    update_xml_files( 
-        src_root_path,
-        dest_root_path,
-        has_aux_well,
-    )
-
-
-    os.system("pause")
 
 
 if __name__  == '__main__':
@@ -118,5 +123,7 @@ if __name__  == '__main__':
     src_root_path = args.src_root_path
     if src_root_path == 'ASK':
         src_root_path = input("Enter the root path of the digitalized files: ")
-
-    handleFilesInFolder(src_root_path)
+    if not src_root_path:
+        print('Invalid path!')
+    else:
+        handleFilesInFolder(src_root_path)
